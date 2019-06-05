e92ce6b677ae5049cc3e66737a9f2b6d346dcd24
wesley leite <leitewesleyleite@gmail.com>
2019-06-05 08:35:19 -0300

    ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Projeto criado em angular';
+  cursos: string[] = [];
+  constructor(){
+    this.cursos[0] = 'abc';
+  }
+  ngOnInit(){
+  }
 }
+
+
